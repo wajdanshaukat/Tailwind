0c9db85dba36b1a6a4aeaba8c985fff40ff9ac3d
wajdanshaukat <wajdanshaukat927@gmail.com>
2024-10-24 15:09:41 +0500

    Responsive form with Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      screens: {
+        '625px': '625px',
+      },
+    },
+  },
+  plugins: [],
+}
+
